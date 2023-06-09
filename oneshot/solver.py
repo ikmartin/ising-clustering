03fa010a5eb6a481082ce97a58d5759bd28a94a1
Isaac Martin <ikmarti@callisto.lps.umd.edu>
2023-06-09 11:06:11 -0400

    More constraint reduction code from Isaac

--- oneshot/solver.py
+++ oneshot/solver.py
@@ -1,11 +1,12 @@
-from ortools.linear_solver.pywraplp import Solver 
+from ortools.linear_solver.pywraplp import Solver
 import torch
 from time import perf_counter as pc
 from numba import jit
 import numpy as np
 
+
 class LPWrapper:
-    def __init__(self, keys, fancy = True):
+    def __init__(self, keys, fancy=True):
         self.keys = keys
         self.threshold = 1e-2
         self.fancy = fancy
@@ -14,7 +15,7 @@ class LPWrapper:
         else:
             self.solver, self.variables = just_solve(keys)
             self.bans = []
-        
+
     def _clear(self):
         for ban in self.bans:
             ban.Clear()
@@ -22,16 +23,16 @@ class LPWrapper:
     def _ban(self, index):
         self.bans[index].SetCoefficient(self.variables[index], 1)
 
-    def solve(self, bans = None):
+    def solve(self, bans=None):
         self._clear()
         if bans is not None:
             for ban in bans:
                 self._ban(ban)
 
         status = self.solver.Solve()
-        if status == 2:     # Infeasible
+        if status == 2:  # Infeasible
             return None
-        
+
         if self.fancy:
             answer = np.array([var.solution_value() for var in self.variables])
             answer[abs(answer) < self.threshold] = 0
@@ -41,29 +42,32 @@ class LPWrapper:
 
     def add_constraints(self, M):
         constraint_vals = torch.t(torch.cat([M.indices(), M.values().unsqueeze(0)]))
-        constraints = [self.solver.Constraint(1.0, self.solver.infinity()) for _ in range(M.shape[0])]
+        constraints = [
+            self.solver.Constraint(1.0, self.solver.infinity())
+            for _ in range(M.shape[0])
+        ]
 
         for x, y, val in constraint_vals:
             constraints[x].SetCoefficient(self.variables[y], int(val))
+
 
 def just_solve(keys):
     num_vars = len(keys)
     solver = Solver.CreateSolver("GLOP")
     inf = solver.infinity()
-    variables = [
-        solver.NumVar(-inf, inf, f"x_{i}") for i in range(num_vars)
-    ]
+    variables = [solver.NumVar(-inf, inf, f"x_{i}") for i in range(num_vars)]
 
     return solver, variables
 
-def build_solver(keys, regularize_low_terms = False):
+
+def build_solver(keys, regularize_low_terms=False):
     """
     Builds a GLOP solver for L1 regularization, without adding any constraints.
     """
 
     solver, variables = just_solve(keys)
     inf = solver.infinity()
-    ban_constraints = [solver.Constraint(0, 0) for var in variables]    
+    ban_constraints = [solver.Constraint(0, 0) for var in variables]
 
     y_vars = [solver.NumVar(-inf, inf, f"y_{var}") for var in variables]
     for x, y in zip(variables, y_vars):
@@ -80,6 +84,5 @@ def build_solver(keys, regularize_low_terms = False):
             objective.SetCoefficient(var, 1)
 
     objective.SetMinimization()
-    
-    return solver, variables, ban_constraints
 
+    return solver, variables, ban_constraints
