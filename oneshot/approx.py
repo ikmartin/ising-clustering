0c6a4aa470c9f49697ecd21e94b65a14373ea459
Andrew Moore <agmoore@utexas.edu>
2023-07-10 19:01:06 -0400

    neutralizable threshold grad descent

--- oneshot/approx.py
+++ oneshot/approx.py
@@ -8,6 +8,7 @@ from ast import literal_eval
 import matplotlib.pyplot as plt
 from new_constraints import constraints
 from fast_constraints import batch_tensor_power, batch_vspin
+from mysolver_interface import call_my_solver
 
 torch.set_printoptions(threshold=50000)
 
@@ -31,9 +32,12 @@ class Model(nn.Module):
 
         i_right = torch.cat([x,y.unsqueeze(1)], dim=-1)
         i_wrong = torch.cat([x,-y.unsqueeze(1)], dim=-1)
+
+        w1 = self.thresh(i_right)
+        w2 = self.thresh(i_wrong)
         
-        t1 = self.s(self.gamma * self.thresh(i_right))
-        t2 = self.s(self.gamma * self.thresh(i_wrong))
+        t1 = self.s(self.gamma * w1)
+        t2 = self.s(self.gamma * w2)
 
         b1 = torch.cat([i_right, t1], dim=-1)
         b2 = torch.cat([i_wrong, t2], dim=-1)
@@ -42,29 +46,44 @@ class Model(nn.Module):
         v2 = batch_vspin(b2, 2)
 
         diff = self.main(v1-v2) + 1
-        penalty = torch.sum((self.gamma/300) * ((1-t1**2)**2 + (1-t2**2)**2))
+        int_penalty = torch.sum((self.gamma/300) * ((1-t1**2)**2 + (1-t2**2)**2))
+        neut_penalty = torch.sum((t2 * w2 - t1 * w1) ** 2)
 
-        print(f'penalty = {penalty}')
-        return diff, penalty
+        print(f'int_penalty = {int_penalty} neut_penalty = {neut_penalty}')
+        """
+        nearest_t1 = torch.sign(self.thresh(i_right))
+        nearest_t2 = torch.sign(self.thresh(i_wrong))
+        nearest_b1 = torch.cat([i_right, nearest_t1], dim=-1)
+        nearest_b2 = torch.cat([i_wrong, nearest_t2], dim=-1)
+        obj = call_my_solver((batch_vspin(nearest_b2, 2)-batch_vspin(nearest_b1, 2)).clone().detach().to_sparse_csc())
+        print(f'obj = {obj}')
+        """
+        return diff, int_penalty, neut_penalty
 
-def main():
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-    model = Model(16)
-
-    _, _, correct = constraints(
+def get_constraints(planes, ising):
+    return constraints(
         4,
         4,
         radius=1,
         degree=1,
         desired=(4,),
         included=(7,),
-        ising=True
+        ising=ising,
+        hyperplanes = planes
     )
+
+
+def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    model = Model(12).to(device)
+
+    _, _, correct = get_constraints(None, True)
+    correct = correct.to(device)
 
     optimizer = torch.optim.SGD(
         params = model.parameters(),
-        lr = 1e-3,
+        lr = 1e-4,
         weight_decay = 1e-5,
         momentum = 0.9,
         nesterov = True
@@ -73,15 +92,15 @@ def main():
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1000, verbose = True)
 
     constraint_loss = nn.Softplus()
-    rho_loss = nn.ReLU()
+    rho_loss = nn.ReLU().to(device)
 
 
     for epoch in range(100000):
 
-        rho, penalty = model(correct)
+        rho, int_penalty, neut_penalty = model(correct)
         closs = constraint_loss(rho)
         rhosum = sum(rho_loss(rho))
-        loss = rhosum + penalty
+        loss = rhosum + int_penalty + neut_penalty
 
         model.setgamma(epoch/100)
 
@@ -90,12 +109,16 @@ def main():
         if loss < 1e-6:
             print(model.thresh.weight)
             print(model.thresh.bias)
+            planes = [(w,b.item()) for w,b in zip(model.thresh.weight.clone().detach(), model.thresh.bias)]
+            M, _, _ = get_constraints(planes, True)
+            objective = call_my_solver(M.to_sparse_csc())
+            print(objective)
             exit()
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        scheduler.step()
+        #scheduler.step()
 
 
 if __name__ == '__main__':
